a9782b08c4249ec7d592c2772ed143ac57e0dd44
Amine Benslimane <amine.benslimane@etu.sorbonne-universite.fr>
2021-10-23 23:12:31 +0200

    cv's service updated

--- src/main/java/com/daar/elasticsearch/service/CvService.java
+++ src/main/java/com/daar/elasticsearch/service/CvService.java
@@ -1,32 +1,67 @@
 package com.daar.elasticsearch.service;
 
+import com.daar.elasticsearch.indices.Index;
 import com.daar.elasticsearch.model.Cv;
-import com.daar.elasticsearch.repository.CvRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.rest.RestStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class CvService {
-    private static final String ID_NOT_FOUND_MSG = "ID %s not found !" ;
-    private final CvRepository cvRepository;
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final Logger LOG = LoggerFactory.getLogger(IndexService.class);
+
+    private final RestHighLevelClient client;
 
     @Autowired
-    public CvService(CvRepository cvRepository) {
-        this.cvRepository = cvRepository;
+    public CvService(RestHighLevelClient client) {
+        this.client = client;
     }
 
-    public void save(final Cv cv){
-        cvRepository.save(cv);
-        System.out.println("CV crée avec l'indentifiant : "+cv.getId());
+    public Boolean index(final Cv cv) {
+        try {
+            final String cvAsString = MAPPER.writeValueAsString(cv);
+
+            final IndexRequest request = new IndexRequest(Index.CV_INDEX);
+            request.id(cv.getId());
+            request.source(cvAsString, XContentType.JSON);
+
+            final IndexResponse reponse = client.index(request, RequestOptions.DEFAULT);
+
+            return reponse != null && reponse.status().equals(RestStatus.OK);
+
+
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            return false;
+        }
     }
 
-    public Cv findById(final String id){
-        return cvRepository
-                .findById(id)
-                .orElseThrow( () -> new IllegalArgumentException(String.format(ID_NOT_FOUND_MSG, id)));
-    }
+    public Cv getById(final String id) {
+        try {
+            final GetResponse documentFields = client.get(
+                    new GetRequest(Index.CV_INDEX, id),
+                    RequestOptions.DEFAULT
+            );
+            if (documentFields == null || documentFields.isSourceEmpty())
+                return null;
 
-    public Iterable<Cv> findAllCv(){
-        return cvRepository.findAll();
+            return MAPPER.readValue(documentFields.getSourceAsString(), Cv.class);
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            return null;
+        }
     }
 }
