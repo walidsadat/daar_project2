7a9ec3255deaf599a0ef8e2092a383d2df8a0df1
Amine Benslimane <amine.benslimane@etu.sorbonne-universite.fr>
2021-10-24 02:47:55 +0200

    adding search feature to the service

--- src/main/java/com/daar/elasticsearch/service/CvService.java
+++ src/main/java/com/daar/elasticsearch/service/CvService.java
@@ -2,21 +2,29 @@ package com.daar.elasticsearch.service;
 
 import com.daar.elasticsearch.indices.Index;
 import com.daar.elasticsearch.model.Cv;
+import com.daar.elasticsearch.search.SearchCvRequest;
+import com.daar.elasticsearch.search.engine.SearchEngine;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 @Service
 public class CvService {
@@ -29,6 +37,33 @@ public class CvService {
     public CvService(RestHighLevelClient client) {
         this.client = client;
     }
+
+    public List<Cv> search(final SearchCvRequest request){
+        final SearchRequest searchRequest = SearchEngine.buildSearchRequest(
+                Index.CV_INDEX,
+                request
+        );
+
+        try {
+            final SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+
+            final SearchHit[] searchHits = response.getHits().getHits();
+            final List<Cv> cvList = new ArrayList<>(searchHits.length);
+
+            for(SearchHit hit : searchHits){
+                cvList.add(
+                        MAPPER.readValue(hit.getSourceAsString(), Cv.class)
+                );
+            }
+
+            return cvList;
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            return Collections.emptyList();
+        }
+    }
+
+
 
     public Boolean index(final Cv cv) {
         try {
