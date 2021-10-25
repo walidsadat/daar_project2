3cef026b318bd4eb9805b5268f656166640e34ac
Amine Benslimane <amine.benslimane@etu.sorbonne-universite.fr>
2021-10-25 17:44:27 +0200

    add sorting fields

--- src/main/java/com/daar/elasticsearch/search/SearchCvRequest.java
+++ src/main/java/com/daar/elasticsearch/search/SearchCvRequest.java
@@ -1,10 +1,14 @@
 package com.daar.elasticsearch.search;
 
+import org.elasticsearch.search.sort.SortOrder;
+
 import java.util.List;
 
 public class SearchCvRequest {
     private List<String> fields;
     private String searchTerm;
+    private SortOrder sortOrder;
+    private String sortBy;
 
     public List<String> getFields(){
         return fields;
@@ -20,5 +24,21 @@ public class SearchCvRequest {
 
     public void setSearchTerm(String searchTerm){
         this.searchTerm = searchTerm;
+    }
+
+    public SortOrder getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(SortOrder sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public String getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(String sortBy) {
+        this.sortBy = sortBy;
     }
 }
