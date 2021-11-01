5f08d89cdd2744964326a4e090c2031785a958c8
Amine Benslimane <amine.benslimane@etu.sorbonne-universite.fr>
2021-11-01 20:05:07 +0100

    use inputStream instead of getFile for jar execution

--- src/main/java/com/daar/elasticsearch/indices/Helper.java
+++ src/main/java/com/daar/elasticsearch/indices/Helper.java
@@ -3,17 +3,21 @@ package com.daar.elasticsearch.indices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
 
-import java.io.File;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 
 public class Helper {
     public static final Logger LOG = LoggerFactory.getLogger(Helper.class);
 
+
     public static String loadAsString(final String path) {
         try {
-            final File resource = new ClassPathResource(path).getFile();
-            return new String(Files.readAllBytes(resource.toPath()));
+            // we use getInputStream instead of getFile for good execution with jar
+            // see https://stackoverflow.com/questions/14876836/file-inside-jar-is-not-visible-for-spring
+            final InputStream resource = new ClassPathResource(path).getInputStream();
+            return StreamUtils.copyToString(resource, Charset.defaultCharset());
         } catch (final Exception e) {
             LOG.error(e.getMessage(), e);
             return null;
